c995c31f910a0d4c7a9a73bede664ebcdfb2ac1a
alexfelipe <alexfelipevieira@gmail.com>
2017-08-04 16:43:50 -0300

    652-csharpcollections-video2.1-Introdução às Listas
    652-csharpcollections-video2.1-Introdução às Listas

---
+++ A12Listas/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A12Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aulaIntro = "Introdução às Coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com Conjuntos";
+
+            //List<string> aulas = new List<string>
+            //{
+            //    aulaIntro,
+            //    aulaModelando,
+            //    aulaSets
+            //};
+
+            List<string> aulas = new List<string>();
+            aulas.Add(aulaIntro);
+            aulas.Add(aulaModelando);
+            aulas.Add(aulaSets);
+
+            Imprimir(aulas);
+
+            Console.WriteLine("A primeira aula é " + aulas[0]);
+            Console.WriteLine("A primeira aula é " + aulas.First());
+            
+            Console.WriteLine("A última aula é " + aulas[aulas.Count - 1]);
+            Console.WriteLine("A última aula é " + aulas.Last());
+
+        }
+
+        private static void Imprimir(List<string> aulas)
+        {
+            //foreach (var aula in aulas)
+            //{
+            //    Console.WriteLine(aula);
+            //}
+
+            //for (int i = 0; i < aulas.Count; i++)
+            //{
+            //    Console.WriteLine(aulas[i]);
+            //}
+
+            aulas.ForEach(aula => 
+            {
+                Console.WriteLine(aula);
+            });
+        }
+    }
+}
+
