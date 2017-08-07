3111ef9dc2d9c19bd40203ff693ff42c429a868f
marcelooliveira <mclricardo@gmail.com>
2017-08-07 00:48:32 -0300

    .
    .

--- A22ListasDeObjetos/Aula.cs
+++ A22ListasDeObjetos/Aula.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace A22ListasDeObjetos
 {
-    //class Aula
+    ////class Aula
+    ////{
+    ////    private string titulo;
+    ////    private int tempo;
+
+    ////    public string Titulo
+    ////    {
+    ////        get { return titulo; }
+    ////        set { titulo = value; }
+    ////    }
+
+    ////    public int Tempo
+    ////    {
+    ////        get { return tempo; }
+    ////        set { tempo = value; }
+    ////    }
+
+    ////    public Aula(string titulo, int tempo)
+    ////    {
+    ////        this.titulo = titulo;
+    ////        this.tempo = tempo;
+    ////    }
+
+    ////    public override string ToString()
+    ////    {
+    ////        return $"[Aula: {this.titulo}, {this.tempo} minutos]";
+    ////    }
+    ////}
+
+    //class Aula : IComparable
     //{
     //    private string titulo;
     //    private int tempo;
@@ -33,41 +62,12 @@ namespace A22ListasDeObjetos
     //    {
     //        return $"[Aula: {this.titulo}, {this.tempo} minutos]";
     //    }
+
+    //    public int CompareTo(object obj)
+    //    {
+    //        Aula that = obj as Aula;
+    //        return this.titulo.CompareTo(that.titulo);
+    //    }
     //}
-
-    class Aula : IComparable
-    {
-        private string titulo;
-        private int tempo;
-
-        public string Titulo
-        {
-            get { return titulo; }
-            set { titulo = value; }
-        }
-
-        public int Tempo
-        {
-            get { return tempo; }
-            set { tempo = value; }
-        }
-
-        public Aula(string titulo, int tempo)
-        {
-            this.titulo = titulo;
-            this.tempo = tempo;
-        }
-
-        public override string ToString()
-        {
-            return $"[Aula: {this.titulo}, {this.tempo} minutos]";
-        }
-
-        public int CompareTo(object obj)
-        {
-            Aula that = obj as Aula;
-            return this.titulo.CompareTo(that.titulo);
-        }
-    }
 
 }
--- A22ListasDeObjetos/Program.cs
+++ A22ListasDeObjetos/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,58 +10,94 @@ namespace A22ListasDeObjetos
     {
         static void Main(string[] args)
         {
-            Aula aulaIntro = new Aula("Introdução às Coleções", 21);
-            Aula aulaModelando = new Aula("Modelando a Classe Aula", 15);
-            Aula aulaSets = new Aula("Trabalhando com Conjuntos", 13);
-
+            //string aulaIntro = "Introdução às Coleções";
+            //string aulaModelando = "Modelando a Classe Aula";
+            //string aulaSets = "Trabalhando com Conjuntos";
+            ///<image url='$(ProjectDir)\Slides\image01.png'/>
+            var aulaIntro = new Aula("Introdução às Coleções", 15);
+            var aulaModelando = new Aula("Modelando a Classe Aula", 16);
+            var aulaSets = new Aula("Trabalhando com Conjuntos", 17);
+            ///<image url='$(ProjectDir)\Slides\image02.png'/>
             //List<Aula> aulas = new List<Aula>();
             //aulas.Add(aulaIntro);
             //aulas.Add(aulaModelando);
             //aulas.Add(aulaSets);
-
-            //A inicialização acima vai ser simplificada abaixo
-
+            ///<image url='$(ProjectDir)\Slides\image03.png'/>
             List<Aula> aulas = new List<Aula>
-             {
-                 aulaIntro,
-                 aulaModelando,
-                 aulaSets
-             };
-
-            ////a próxima linha gera um erro de compilação!
-            //aulas.Add("Equals e HashCode");
-
-            foreach (var aula in aulas)
             {
-                Console.WriteLine(aula); //não exibe detalhes da aula, pois precisamos implementar ToString()
-            }
+                aulaIntro, aulaModelando, aulaSets
+            };
 
-            //Após implementarmos ToString() em Aula, a linha acima exibe corretamente a aula
+            ///<image url='$(ProjectDir)\Slides\image04.png'/>
+            //aulas.Add("Conclusão");
 
+            ///<image url='$(ProjectDir)\Slides\image05.png'/>
+            Imprimir(aulas);
+            ///<image url='$(ProjectDir)\Slides\image06.png'/>
+
+            ///<image url='$(ProjectDir)\Slides\image07.png'/>
             aulas.Sort();
-            ////Exceção Sem Tratamento: System.InvalidOperationException: 
-            //Falha ao comparar dois elementos na matriz. --->System.ArgumentException: 
-            //Pelo menos um objeto deve implementar IComparable.
+///<image url='$(ProjectDir)\Slides\image08.png'/>
+            
+///<image url='$(ProjectDir)\Slides\image09.png'/>
+            Imprimir(aulas);
 
-            ImprimeAulas(aulas);
-
-            //vamos ordenar de outro jeito agora
+            ///<image url='$(ProjectDir)\Slides\image10.png'/>
             aulas.Sort((este, outro) => este.Tempo.CompareTo(outro.Tempo));
-
-            ImprimeAulas(aulas);
-
-///<image url='$(ProjectDir)\Slides\image1.png'/>
-
+            ///<image url='$(ProjectDir)\Slides\image11.png'/>
+            Imprimir(aulas);
         }
 
-        private static void ImprimeAulas(List<Aula> aulas)
+        private static void Imprimir(List<Aula> aulas)
         {
             Console.Clear();
             foreach (var aula in aulas)
             {
-                Console.WriteLine(aula); //não exibe detalhes da aula, pois precisamos implementar ToString()
+                Console.WriteLine(aula);
             }
         }
     }
+
+    internal class Aula : IComparable
+    {
+        private string titulo;
+        private int tempo;
+
+        public Aula(string titulo, int tempo)
+        {
+            this.titulo = titulo;
+            this.tempo = tempo;
+        }
+
+        public string Titulo { get => titulo; set => titulo = value; }
+        public int Tempo { get => tempo; set => tempo = value; }
+
+        public int CompareTo(object obj)
+        {
+            var other = obj as Aula;
+            return this.titulo.CompareTo(other.titulo);
+        }
+
+        public override string ToString()
+        {
+            return $"[titulo: {titulo} - duração: {tempo} minutos]";
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
