3c16f25a5d20a68c0eff6393f9c2144708b4c3b1
marcelooliveira <mclricardo@gmail.com>
2017-08-08 20:02:00 -0300

    .
    .

---
+++ A32SetNoModelo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A32SetNoModelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpColecoes = new Curso("Dominando as coleções do C#", "Marcelo Oliveira");
+
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            csharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
+            csharpColecoes.Adiciona(new Aula("Modelando com Coleções", 24));
+
+            Aluno a1 = new Aluno("Vanessa Tonini", 34672);
+            Aluno a2 = new Aluno("Ana Losnak", 5617);
+            Aluno a3 = new Aluno("Rafael Nercessian", 17645);
+
+            csharpColecoes.Matricula(a1);
+            csharpColecoes.Matricula(a2);
+            csharpColecoes.Matricula(a3);
+
+            Console.WriteLine("Todos os alunos matriculados: ");
+            csharpColecoes.Alunos.ToList().ForEach(aluno => {
+                Console.WriteLine(aluno);
+            });
+        }
+    }
+}
