10201bf3287ac924b431ef25ed2f8a0dbd54a26f
alexfelipe <alexfelipevieira@gmail.com>
2017-08-08 15:37:32 -0300

    652-csharpcollections-video2.5-Ordenando e Totalizando
    652-csharpcollections-video2.5-Ordenando e Totalizando

---
+++ A25OrdenandoESomando/Tasks.cs
@@ -0,0 +1,26 @@
+﻿//TODO: Nova aula: Criando uma Aula, tempo = 20 minutos
+//TODO: Nova aula: Modelando com Coleções, tempo = 19 minutos
+
+//TODO: Imprimir (de novo) as aulas do curso
+
+//TODO: ordenar as aulas do curso (ordem alfabética)
+
+//TODO: copiar para outra lista e ordenar
+
+//TODO: imprimir a cópia
+
+//TODO: totalizar tempo do curso
+
+//TODO: imprimir duração total do curso
+
+//TODO: somar com LINQ (Language Integrated Queries)
+
+//TODO: imprimir Curso
+
+//TODO: sobreescrever ToString(), imprimir Curso
+
+
+
+
+
+
