96949825a1a0e3b8f47854cd858b224457312ea2
alexfelipe <alexfelipevieira@gmail.com>
2017-08-09 15:12:10 -0300

    .
    .

--- A33EqualsEHashCode/Program.cs
+++ A33EqualsEHashCode/Program.cs
@@ -10,37 +10,50 @@ namespace A33EqualsEHashCode
     {
         static void Main(string[] args)
         {
-            Curso csharpColecoes = new Curso("Dominando as coleções do C#", "Marcelo Oliveira");
-
+            //vamos declarar o curso
+            Curso csharpColecoes = new Curso("C# Colecoes", "Marcelo Oliveira");
+            //...e adicionar 3 aulas a esse curso
+            //Trabalhando com Listas - 21 minutos;
+            //Criando uma Aula - 20 minutos
+            //Modelando com Coleções - 24 minutos
             csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
             csharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
             csharpColecoes.Adiciona(new Aula("Modelando com Coleções", 24));
+            //um aluno também tem matrícula!
+            //vamos criar a class Aluno com Nome e NumeroMatricula
 
+            //Instanciando 3 alunos com suas matrícula:
+            //Vanessa Tonini - 34672
+            //Ana Losnak - 5617
+            //Rafael Nercessian - 17645
             Aluno a1 = new Aluno("Vanessa Tonini", 34672);
             Aluno a2 = new Aluno("Ana Losnak", 5617);
             Aluno a3 = new Aluno("Rafael Nercessian", 17645);
-
+            //Precisamos Matricular os alunos no curso, criando um método
             csharpColecoes.Matricula(a1);
             csharpColecoes.Matricula(a2);
             csharpColecoes.Matricula(a3);
 
-            Console.WriteLine("Todos os alunos matriculados: ");
-            csharpColecoes.Alunos.ToList().ForEach(aluno => {
+            //Imprimindo os alunos matriculados
+            Console.WriteLine("Imprimindo os alunos matriculados");
+            foreach (var aluno in csharpColecoes.Alunos)
+            {
                 Console.WriteLine(aluno);
-            });
+            }
+            //no formato "[Nome: , Matrícula: ]"
 
             ///Imprimir: "O aluno a3 está matriculado?"
-            
+
             ///Criar um método EstaMatriculado
-            
+
             ///Vamos instanciar uma aluna (Vanessa Tonini)
-            
+
             ///e verificar se Tonini está matriculada
-            
+
             ///Mas a1 == a Tonini?
-            
+
             ///O que ocorreu? a1 é equals a Tonini?
-            
+
             ///São diferentes! Precisamos então implementar Equals()
 
             ///Agora fazemos a1 Equals tonini
@@ -59,7 +72,7 @@ namespace A33EqualsEHashCode
             ///PORÉM, o contrário não é verdadeiro: 
             ///Dois objetos com mesmo hash codes não são necessariamente iguais! 
 
-            
+
         }
     }
 }
