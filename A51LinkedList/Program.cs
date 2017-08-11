8f7d2d8efe9f7761f71d1faf0d96fdd61f7affe7
marcelooliveira <mclricardo@gmail.com>
2017-08-11 12:44:49 -0300

    ..
    ..

--- A51LinkedList/Program.cs
+++ A51LinkedList/Program.cs
@@ -10,32 +10,69 @@ namespace A51LinkedList
     {
         static void Main(string[] args)
         {
+            //Imagine uma lista de frutas
             List<string> frutas = new List<string>()
             {
                 "abacate", "banana", "caqui", "damasco", "figo"
             };
+
+            Console.Clear();
+            foreach (var f in frutas)
+            {
+                Console.WriteLine(f);
+            }
             ///<image url="$(ProjectDir)\Slides\image1.png" scale=""/>
+            //Adicionar um elemento ao final de uma lista é fácil
 
-            foreach (var f in frutas)
-            {
-                Console.WriteLine(f);
-            }
-
+            //Porém inserir no meio da lista exige mais esforço computacional
+            
             frutas.Insert(2, "caju");
-
+            Console.Clear();
+            foreach (var f in frutas)
+            {
+                Console.WriteLine(f);
+            }
             ///<image url="$(ProjectDir)\Slides\image2.png" scale=""/>
+            //porque os elementos têm que ser deslocados para darem
+            //espaço ao novo elemento!
+
+            //e se tivermos que remover esse elemento, os elementos
+            //seguintes precisam ser deslocados de novo!
+
+            Console.WriteLine();
+            frutas.Remove("caju");
             foreach (var f in frutas)
             {
                 Console.WriteLine(f);
             }
 
+            //Quanto maior a lista, mais ineficiente é a inserção
+            //e remoção de elementos no meio dela!
+
+            //Que coleção é mais apropriada para inserção/remoção rápida?
+            //Apresentando LISTA LIGADA (LINKED LIST):
+            //- Elementos não precisam estar em sequência em memória
+            //- Cada elemento sabe quem é o anterior e o próximo
+            //- Cada elemento é um "nó" que contém um valor
             ///<image url="$(ProjectDir)\Slides\linked.png" scale=""/>
 
+            //Como a ordem é mantida? Usando ponteiros
             ///<image url="$(ProjectDir)\Slides\linked2.png" scale=""/>
+            //Instanciando uma nova lista ligada: dias da semana
             LinkedList<string> dias = new LinkedList<string>();
-
+            
+            //Adicionando um dia qualquer: "quarta"
             var d4 = dias.AddFirst("quarta");
+
+            //"quarta" é o primeiro elemento da lista ligada
             ///<image url="$(ProjectDir)\Slides\image3.png" scale=""/>
+            //cada elemento é um nó: LinkedListNode<T>
+            //System.Collections.Generic.LinkedListNode`1[System.String]
+            Console.WriteLine(d4.GetType());
+
+            //Mas e o valor "quarta"? Está na propriedade d4.Value
+            Console.WriteLine("d4.Value: " + d4.Value);
+
             var d2 = dias.AddBefore(d4, "segunda");
             ///<image url="$(ProjectDir)\Slides\image4.png" scale=""/>
             var d3 = dias.AddAfter(d2, "terça");
