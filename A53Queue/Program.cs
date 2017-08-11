baa8d95b9fae8dc5d9dc3480e26f362b9f97bf71
alexfelipe <alexfelipevieira@gmail.com>
2017-08-11 17:47:27 -0300

    .
    .

--- A53Queue/Program.cs
+++ A53Queue/Program.cs
@@ -8,76 +8,25 @@ namespace A53Queue
 {
     class Program
     {
-        static Queue<string> pedagio = new Queue<string>();
         static void Main(string[] args)
         {
-            //string veiculo = "van";
-            //Console.WriteLine($"Entrou na fila: {veiculo}");
-            //pedagio.Enqueue(veiculo);
-            //foreach (var v in pedagio)
-            //{
-            //    Console.WriteLine(v);
-            //}
+            ///entrou: van            
 
-            //string veiculo = "van";
-            //Enfileirar(veiculo);
+            ///entrou: kombi
+            
+            ///entrou: guincho
+            
+            ///entrou: pickup
+            
 
-            Enfileirar("van");
-            ///<image url="$(ProjectDir)\Slides\queue1.png" scale=""/>
-
-            Enfileirar("kombi");
-            ///<image url="$(ProjectDir)\Slides\queue2.png" scale=""/>
-            Enfileirar("guincho");
-            ///<image url="$(ProjectDir)\Slides\queue3.png" scale=""/>
-            Enfileirar("pickup");
-            ///<image url="$(ProjectDir)\Slides\queue4.png" scale=""/>
-
-            Desenfileirar();
-            ///<image url="$(ProjectDir)\Slides\queue5.png" scale=""/>
-            Desenfileirar();
-            ///<image url="$(ProjectDir)\Slides\queue6.png" scale=""/>
-            Desenfileirar();
-            ///<image url="$(ProjectDir)\Slides\queue7.png" scale=""/>
-            Desenfileirar();
-            Desenfileirar();
-        }
-
-        private static void Desenfileirar()
-        {
-            if (pedagio.Any())
-            {
-                if (pedagio.Peek() == "guincho")
-                {
-                    Console.WriteLine("Guincho está esperando no pedágio.");
-                }
-                var veiculo = pedagio.Dequeue();
-                Console.WriteLine($"Saiu da fila: {veiculo}");
-                ImprimeFila();
-            }
-        }
-
-        private static void Enfileirar(string veiculo)
-        {
-            //Console.WriteLine($"Entrou na fila: {veiculo}");
-            //pedagio.Enqueue(veiculo);
-            //foreach (var v in pedagio)
-            //{
-            //    Console.WriteLine(v);
-            //}
-
-            Console.WriteLine($"Entrou na fila: {veiculo}");
-            pedagio.Enqueue(veiculo);
-            ImprimeFila();
-        }
-
-        private static void ImprimeFila()
-        {
-            Console.WriteLine("FILA:");
-            foreach (var v in pedagio)
-            {
-                Console.WriteLine(v);
-            }
-            Console.WriteLine();
+            ///carro liberado
+            
+            ///carro liberado
+            
+            ///carro liberado
+            
+            ///carro liberado
+            ///carro liberado
         }
     }
 }
