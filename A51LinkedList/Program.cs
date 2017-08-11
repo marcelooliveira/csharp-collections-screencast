3601b4fc399fbec4e0742ff843dd9c79609d3c64
marcelooliveira <mclricardo@gmail.com>
2017-08-11 12:48:45 -0300

    .
    .

--- A51LinkedList/Program.cs
+++ A51LinkedList/Program.cs
@@ -73,24 +73,50 @@ namespace A51LinkedList
             //Mas e o valor "quarta"? Está na propriedade d4.Value
             Console.WriteLine("d4.Value: " + d4.Value);
 
+            //E para adicionar mais itens? LinkedList não possui Add!
+            //Podemos adicionar de 4 formas:
+            //1. Como primeiró nó
+            //2. Como último nó
+            //3. Antes de um nó conhecido
+            //4. Depois de um nó conhecido
+
+            //Vamos adicionar segunda, antes de quarta:
             var d2 = dias.AddBefore(d4, "segunda");
             ///<image url="$(ProjectDir)\Slides\image4.png" scale=""/>
+            //Agora d2 e d4 estão ligados!
+            //d2.Next é igual a d4
+            //d4.Previous é igual a d2
+
+            //Continuando com nossa lista ligada, 
+            //vamos adicionar terça depois de segunda
             var d3 = dias.AddAfter(d2, "terça");
             ///<image url="$(ProjectDir)\Slides\image5.png" scale=""/>
+            //Perceba que os "ponteiros", ou referências
+            //de d2 e d4 foram redirecionados para d3!!
+            
+            //vamos adicionar sexta depois de quarta
             var d6 = dias.AddAfter(d4, "sexta");
             ///<image url="$(ProjectDir)\Slides\image6.png" scale=""/>
+            //sábado depois de sexta
             var d7 = dias.AddAfter(d6, "sábado");
             ///<image url="$(ProjectDir)\Slides\image7.png" scale=""/>
+            //quinta antes de sexta
             var d5 = dias.AddBefore(d6, "quinta");
             ///<image url="$(ProjectDir)\Slides\image8.png" scale=""/>
+            //domingo antes de segunda
             var d1 = dias.AddBefore(d2, "domingo");
             ///<image url="$(ProjectDir)\Slides\image9.png" scale=""/>
-
+            //Agora vamos imprimir a lista ligada
             foreach (var d in dias)
             {
                 Console.WriteLine(d);
             }
 
+            //LinkedList NÃO DÁ suporte ao acesso de índice: dias[0]
+            //por isso podemos far um laço foreach mas não um for!
+
+            //Para removermos um elemento, temos que conhecer a
+            //referência do LinkedListNode!
             dias.Remove(d4);
 
             foreach (var d in dias)
